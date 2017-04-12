7fc4b2a79701c338f888d6e75a0a6b8b08c8c57b
YuzhongHuang <yuzhong.huang@students.olin.edu>
2017-04-11 21:40:54 -0400

    add comments; change feedforward img to feedforward video

--- src/networks.py
+++ src/networks.py
@@ -1,51 +1,121 @@
+"""
+Async Neural Network
+~~~~~~~~~~~~~~~~~~~~
+
+Yuzhong Huang
+2017.4.11
+
+A module to implement an async neural network algorithm 
+in a feedforward manner.  Individual neurons has a nucleus 
+and only fires when accumlative nucleus is beyond certain 
+threshold value.  Note that it is not optimized,
+and omits many desirable features.
+"""
+
 import numpy as np
 
 class Network(object):
     def __init__(self, sizes, density, num):
+        """ 
+        The list ``sizes`` contains the unit lengths of respective 
+        dimensions of the network.  The first two dimensions are 
+        consistent with the input image. 
+
+        The integer ``density`` refers to the number output 
+        connections that each neuron contains. 
+
+        The integer ``num`` indicates number of neurons in the 
+        network's hidden layer.  Note that there are #(img pixels) 
+        input neurons in addition to the hidden layer """
+
         self.sizes = sizes
         self.density = density
         self.output = []
-        self.neurons = [Neuron() for i in range(sizes[0]*sizes[1] + num)] # (input neurons + num) of neurons
 
-        # initialize neuron's positions and form connections of neurons
+        #(input layer + hidden layer) of neurons
+        self.neurons = [Neuron() for i in range(sizes[0]*sizes[1] + num)] 
+
+        # initialize signals that store all the intermediate inputs 
+        # to individual neurons, including input neurons
+        self.signals = np.zeros(len(neurons))
+
+        # initialize neuron's positions and form connections
         self.pos = []
         self.init_pos()
         self.form_connection()
 
-        # initialize signals that store all the intermediate inputs to individual neurons
-        self.signals = np.zeros(len(neurons))
 
     def init_pos(self):
+        """
+        Randomly populate the neurons' 3D positions in the space 
+        given by self.sizes """
+
+        # input layer neurons have z value equal to 0
         for i in self.sizes[0]:
             for j in self.sizes[1]:
                 self.pos.append((i,j,0))
 
-        xs = np.random.uniform(low=0, high=sizes[0], size=(num,))
-        ys = np.random.uniform(low=0, high=sizes[1], size=(num,))
-        zs = np.random.uniform(low=0, high=sizes[2], size=(num,))
-        self.pos += ([(x,y,z) for x, y, z in zip(xs, ys, zs)])
+        # randomly generate hidden layer neurons
+        xs = np.random.uniform(low=0, high=sizes[0], size=(self.num,))
+        ys = np.random.uniform(low=0, high=sizes[1], size=(self.num,))
+        zs = np.random.uniform(low=0, high=sizes[2], size=(self.num,))
+        pos = [(x,y,z) for x, y, z in zip(xs, ys, zs)]
+
+        # sort the list based on z value
+        sort(pos, key=lambda: x:x[2]) 
+        self.pos += pos
 
     def form_connection(self):
+        """
+        Connects neurons to form a network. Each neuron connects to 
+        #density of nearest output neurons that has z vaule higher 
+        than itself """
+
+        # loop through all the neurons spatially to form connections
         for i in range(len(self.pos)):
-            self.neurons[i].connect(find_connection(self.pos(i)))
+            self.neurons[i].connect(find_connection(self.pos(i), i))
 
-    def magnitude(pos):
-        return (pos**2).sum()
+    def find_connection(self, pos, i):
+        """
+        Find indices of #density of nearest output neurons that 
+        has z vaule higher than itself. 
 
-    def find_connection(self, pos):
-        l = []
-        for p in self.pos:
-            l.append(numpy.subtract(p, pos))
-        sorted(l, key=magnitude)[:self.density]
+        Tuple ``pos``: 3D postion of the given neuron
 
-    def feedforward(self, img):
-        # store input signals
-        for i in self.sizes[0]:
-            for j in self.sizes[1]:
-                self.signals[i*self.sizes[0]+j] = img[i][j]
+        Integer ``i``: index of the given neuron in self.neurons """
 
-        # feedforward while signals are not zeros       
+        # initialize a list of candidates of connections
+        candidates = []
+
+        # start with neurons with higher z values
+        start = max(i, self.sizes[0]*self.sizes[1]) # in case of input neurons, start from the hidden layer
+
+        # distance is equavalent to magnitude of difference vector
+        for p in self.pos[start:]:
+            candidates.append(numpy.subtract(p, pos))
+
+        # get a list of indices sorted on distance from the given position
+        indices = [i[0] for i in sorted(enumerate(candidates), key=lambda: x:(x[1]**2).sum())]
+
+        # return indices of the first #(self.density) neurons
+        return indices[:self.density] + i
+
+    def feedforward(self, imgs):
+        """
+        Feedforward an array of imgs to the neural network. Given 
+        the network is async, feedforward loop might be longer than
+        video length """
+        
+        vid_length = len(imgs)
+        current = 0
+
+        # feedforward while intermediate signals are not zeros       
         while self.signals.sum():
+            # store input signals if available
+            if current <= vid_length:
+                self.set_input(imgs[current])
+                current += 1
+
             # get the non zeros indices
             non_zeros = [i for i, e in enumerate(self.signals) if e!=0]
 
@@ -54,14 +124,21 @@ class Network(object):
                 # get outputs from a single neuron
                 outputs = self.neurons[i].receive(self.signals[i])
 
-                if self.neurons[i].outs:
+                if self.neurons[i].outs: # check if the neuron is at top of the network
                     # store the outputs to signals if the neuron fires
                     if outputs:
                         # store signals to individual neurons that connected to the neuron
                         for j in range(len(self.neurons[i].outs)):
                             self.signals[self.neurons[i].outs(j)] = outputs[j]
                 else:
+                    # append to output if the neuron is at top of the network
                     self.output.append(outputs)
+
+    def set_input(self, img):
+        """store a single img to input signals"""
+        for i in self.sizes[0]:
+            for j in self.sizes[1]:
+                self.signals[i*self.sizes[0]+j] = img[i][j]
 
 
 class Neuron(object):
